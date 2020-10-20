0593576729ce8eb2b104a80a1800b13eb393cbde
Riddhi Suteri <ridsuteri@gmail.com>
2020-10-20 23:07:42 +0530

    restrict post to user

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
+const passport = require('passport');
+
 const likesController = require('../controllers/posts_controller');
 const postsController = require('../controllers/posts_controller');
 // console.log('hiting likes router');
 
 router.get('/likes', likesController.like);
-router.post('/create', postsController.create);
+router.post('/create',passport.checkAuthentication, postsController.create);
 
 module.exports = router;
