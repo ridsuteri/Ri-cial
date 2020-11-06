c3ebc1766584a818d4a7150afaf7ccbd4445a36d
Riddhi Suteri <ridsuteri@gmail.com>
2020-11-06 22:06:07 +0530

    handle edge case of replacinng avatar

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,7 @@
 const { urlencoded } = require('express');
 const User = require('../models/user');
-
+const fs = require('fs');
+const path = require('path');
 module.exports.profile = function (req, res) {
     User.findById(req.params.id, function (err, user) {
         res.render('user_profile', {
@@ -27,6 +28,10 @@ module.exports.update = async function (req, res) {
                 user.email = req.body.email;
 
                 if (req.file) {
+
+                    if (user.avatar) {
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar));
+                    }
                     // this is saving the path of the uploaded file into the avatar field in the user
                     user.avatar = User.avatarPath + '/' + req.file.filename
                 }
